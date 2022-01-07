9a8adb78ac1a0efb8abc7f3e4f0dd2f3bd9d2652
Stellarsc <95481648+Stellarsc@users.noreply.github.com>
2022-01-07 14:13:54 +0100

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -10,19 +10,36 @@ fenetre = pygame.display.set_mode((640, 480))
 fond = pygame.image.load("image/background.jpg").convert()
 fenetre.blit(fond, (0,0))
 
-
-
+#Chargement et collage du personnage 
 perso = pygame.image.load("image/perso.jpg").convert()
-fenetre.blit(perso, (200,0))
+position_perso = perso.get_rect()
+fenetre.blit(perso, position_perso)
+
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
+
+
 
 #BOUCLE INFINIE
 continuer = True
 
 while continuer:
-	for event in pygame.event.get():  
-		if event.type == QUIT:     
-			continuer = False
+    for event in pygame.event.get():  
+        if event.type == QUIT:     
+            continuer = False
+        if event.type == KEYDOWN:
+            if event.key == K_DOWN:	#Si "flèche bas"
+            #On descend le perso
+                position_perso = position_perso.move(0,3)
+            
+            
+        fenetre.blit(fond, (0,0))	
+        fenetre.blit(perso, position_perso)
+        #Rafraichissement
+        pygame.display.flip()
+            
+            
+        
+
 pygame.quit()
