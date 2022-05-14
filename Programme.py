ac523d72bc6b2811bce6dc3fde21a389870b5907
CharlotteL <95481172+CLorillard@users.noreply.github.com>
2022-05-14 12:23:04 +0200

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -57,6 +57,7 @@ MUSIQUE = pygame.mixer.music.load("Musique/musique.wav")
 PERSONNAGE = pygame.image.load(os.path.join("image", "perso.png"))
 PERSO = pygame.transform.scale(PERSONNAGE, (PERSO_WIDTH, PERSO_HEIGHT))    #redimensionnage du perso
 
+#Chargement d'icones
 NOTICE_1 = pygame.image.load("image/regles.png").convert() #importation de l'image NOTICE
 NOTICE =  pygame.transform.scale(NOTICE_1, (700,300))
 
@@ -106,6 +107,7 @@ for i in range(12):
 
 FPS = 60 #pour pouvoir definir nombre de fois que la boucle tourne par seconde
 
+#Fonction pour le deplacement du personnage
 def perso_mouvement(keys_pressed,perso):
     if keys_pressed[pygame.K_LEFT] and perso.x - DIST > 0:  #si flèche gauche pressée et ne sort pas du cadre
         perso.x -= DIST  #perso se déplace à gauche
@@ -116,11 +118,11 @@ def perso_mouvement(keys_pressed,perso):
     if keys_pressed[pygame.K_DOWN] and perso.y + DIST < HEIGHT - PERSO_HEIGHT:   #si flèche bas pressée et ne sort pas du cadre
         perso.y += DIST    #perso se déplace vers le bas
 
-#fonction principale
+#Fonction principale
 def main():
     perso = pygame.Rect(450, 600, PERSO_WIDTH, PERSO_HEIGHT) #coordonnées du perso avec la fonction rect
     clock = pygame.time.Clock()
-    #pygame.mixer.music.play(-1,0.0)
+    pygame.mixer.music.play(-1,0.0)
     tZero=time.time() #Récupération de tZero
     #initialisation des variables/listes/booléens
     run = True
@@ -194,36 +196,35 @@ def main():
                 print(map_1)
 
                 FENETRE.blit(droit, (0, 0))#affichage de la map de depart
-                if perso.y - DIST <= 0:
-                    if WIDTH-940 <= perso.x <= WIDTH-940+355 and map_haut:
-                        map_gauche+=1
+                if perso.y - DIST <= 0:#si l'ordonnée du personnage est inférieure ou égale a 0
+                    if WIDTH-940 <= perso.x <= WIDTH-940+355 and map_haut:#si l'abscisse du personnage est compris entre 60 et 415
+                        map_gauche+=1 #la variable augmente de 1
 
-                    elif WIDTH-410 <= perso.x <= WIDTH-410+345 and map_haut:
-                        map_droite+=1
-                    map_haut+=1
-                    perso.y = perso.y + 700
+                    elif WIDTH-410 <= perso.x <= WIDTH-410+345 and map_haut:#si l'abscisse du personnage est compris entre 590 et 935
+                        map_droite+=1 #la variable augmente de 1
+                    map_haut+=1 #la variable augmente de 1
+                    perso.y = perso.y + 700 #le personnage retourne en bas 
 
-                if map_haut ==1:
-                    FENETRE.blit(che2, (0, 0))
-                    chemin_2 = 1
+                if map_haut ==1: #si le personnage a touché le bord du haut
+                    FENETRE.blit(che2, (0, 0)) #affichage de la map a 2 issues
+                    chemin_2 = 1 #la variable augmente de 1
 
-                elif map_haut ==2:
-                    if map_gauche == 1 and map_1[i]==1:
-                        FENETRE.blit(cul_de_sac, (0,0))
-                        culdesac = 1
+                elif map_haut ==2: #si le personnage a touché 2 fois le bord du haut
+                    if map_gauche == 1 and map_1[i]==1: 
+                        FENETRE.blit(cul_de_sac, (0,0)) #affichage du cul de sac
+                        culdesac = 1 #la variable augmente de 1
 
                     elif map_gauche == 1 and map_1[i]==2:
-                        FENETRE.blit(toutdroit, (0, 0))
+                        FENETRE.blit(toutdroit, (0, 0)) #affichage d'une map sortie d'intersection
 
                     elif map_droite == 1 and map_1[i]==1:
-                        FENETRE.blit(toutdroit2, (0,0))
+                        FENETRE.blit(toutdroit2, (0,0)) #affichage d'une map sortie d'intersection
 
                     elif map_droite == 1 and map_1[i]==2:
-                        FENETRE.blit(cul_de_sac, (0,0))
-                        culdesac = 1
+                        FENETRE.blit(cul_de_sac, (0,0)) #affichage du cul de sac
+                        culdesac = 1 #la variable augmente de 1
 
-                elif culdesac == 1:
-
+                elif culdesac == 1: #si le personnage est dans un cul de sac
                     FENETRE.blit(game_over, (0, 0))#affichage de l'image Game over
                     FENETRE.blit(TIMEWIN,(WIDTH-700,HEIGHT-500))#affichage de l'image pour le meilleur temps
                     FENETRE.blit(TIME,(WIDTH-685,HEIGHT-525))#affichage de l'image horloge
@@ -233,7 +234,6 @@ def main():
                     i=0 #i revient a 0
 
                     #si on appuie sur "rejouer" les variables retournent à 0
-
                     for event in pygame.event.get():
                         if event.type == pygame.MOUSEBUTTONDOWN:
                             if WIDTH-600 <= mouse[0] <= WIDTH-600+175 and HEIGHT-450 <= mouse[1] <= HEIGHT-450+300 :
@@ -246,8 +246,8 @@ def main():
                                 i = 0
                                 tZero=time.time() #Récupération de tZero
 
-                        elif event.type == pygame.KEYDOWN:#si on appuie sur "Entrée" les variables retournent à 0 et on arrive sur l'écran principal car niveau 1 devient false
-                            if event.key == K_RETURN:
+                        elif event.type == pygame.KEYDOWN:
+                            if event.key == K_RETURN:#si on appuie sur "Entrée" les variables retournent à 0 et on arrive sur l'écran principal car niveau 1 devient false
                                 niveau1 = False
                                 game_over_45 = 0
                                 map_gauche = 0
@@ -260,15 +260,15 @@ def main():
 
 
                 else :
-                    if map_haut != 0:
-                        i += 1
+                    if map_haut != 0: #si le personnage a touché le bord haut au moins une fois
+                        i += 1 #l'indice i augmente de 1
                         map_haut = 0
                         map_droite = 0
                         map_gauche = 0
 
 
-                if i >= 7 :
-                    FENETRE.blit(FINISH, (0,0))
+                if i >= 7 : #si l'indice est superieur ou égale à 7
+                    FENETRE.blit(FINISH, (0,0)) #affichage de la map d'arrivé
                     if event.type == pygame.KEYDOWN:
                         if event.key == K_RETURN:
                             niveau1 = False
@@ -283,16 +283,16 @@ def main():
                                 map_1.append(random.choice([1, 2]))
                             i = 0
 
-                if game_over_45 != 1:
+                if game_over_45 != 1: #si le joeur n'a pas perdu
                     keys_pressed = pygame.key.get_pressed()
                     perso_mouvement(keys_pressed,perso)  #appel fonction mouvement perso
-                    FENETRE.blit(PERSO, (perso.x, perso.y))
+                    FENETRE.blit(PERSO, (perso.x, perso.y)) #affichage de l'avatar
                     Start_time=time.time()#temps depart
-                    Actual_time=Start_time-tZero
+                    Actual_time=Start_time-tZero #calcul du temps
                     texts = smallfont.render(str(round(Actual_time)), True, (255,255,255))#transformation du temps en texte
                     pygame.draw.rect(FENETRE,ROSE,[WIDTH-999,HEIGHT-690,62,30])#creation zone pour affichage temps
-                    FENETRE.blit(texts,(20,10))#affichage du temps
-                    FENETRE.blit(TIME, (0,15))#affichage icon horloge
+                    FENETRE.blit(texts,(20,10))#affichage du chronomètre
+                    FENETRE.blit(TIME, (0,15))#affichage icone horloge
 
             elif niveau2:
 
